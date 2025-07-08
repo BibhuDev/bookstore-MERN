6ce66c7aa5fb3bd8c02187898d501bcc7614c2b8
Bibhudutta panda <bibhuduttapanda7@gmail.com>
2025-07-09 01:01:27 +0530

    react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBook from './pages/ShowBook'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element= {<Home />} />
+      <Route path='/books/create' element={<CreateBook/>} />
+      <Route path='/books/delete/:id' element={<DeleteBook/>} />
+      <Route path='/books/edit/:id' element={<EditBook/>} />
+      <Route path='/books/details/:id' element={<ShowBook/>} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
